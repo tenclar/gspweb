6caef04bd5eb7ca2cda8a6068e4de65817d65a2d
tenclar <tenclarvalus@gmail.com>
2020-07-25 00:34:28 -0500

    Componentes

---
+++ src/components/admin/Header/index.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+
+const Header: React.FC = () => <h1>Header</h1>;
+export default Header;
---
+++ src/components/admin/Input/styles.ts
@@ -0,0 +1,76 @@
+import styled, { css } from 'styled-components';
+import ToolTip from '../Tooltip';
+
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+  isErrored: boolean;
+}
+export const Container = styled.div<ContainerProps>`
+  color: #666360;
+  background: #232129;
+  border-radius: 10px;
+  border: 2px solid #233129;
+  padding: 16px;
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+
+  & + div {
+    margin-top: 8px;
+  }
+
+  ${(props) =>
+    props.isErrored &&
+    css`
+      border-color: #c53030;
+    `}
+
+  ${(props) =>
+    props.isFocused &&
+    css`
+      color: #ff9900;
+      border-color: #ff9000;
+    `}
+
+  ${(props) =>
+    props.isFilled &&
+    css`
+      color: #ff9900;
+    `}
+
+
+
+  input {
+    flex: 1;
+    background: transparent;
+    border: 0;
+    color: #fff;
+
+    &::placeholder {
+      color: #666360;
+    }
+  }
+
+  > svg {
+    margin-right: 16px;
+  }
+`;
+
+export const Error = styled(ToolTip)`
+  height: 20px;
+  margin-left: 16px;
+
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background: #c53030;
+    color: #fff;
+    &::before {
+      border-color: #c53030 transparent;
+    }
+  }
+`;
