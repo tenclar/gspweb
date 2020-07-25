7cdaa766b794f2f661783fd1a9b631acd18cc90c
tenclar <tenclarvalus@gmail.com>
2020-07-25 00:34:50 -0500

    eslint prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import GlobalStyled from './styles/global';
+import Routes from './routes';
 
+const App: React.FC = () => (
+  <>
+    <GlobalStyled />
 
-function App() {
-  return (
-   <h1>Dashboard Admin</h1>
-  );
-}
+    <Router>
+      <Routes />
+    </Router>
+  </>
+);
 
 export default App;
