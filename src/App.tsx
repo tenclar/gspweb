f4f10b2c5d69a9aee0251a3c0678b77603b86174
tenclar <tenclar.silva@ac.gov.br>
2020-11-05 14:05:35 -0500

    Configuração toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import GlobalStyled from './styles/global';
-import { AuthProvider } from './hooks/Auth';
+import AppProvider from './hooks';
 import Routes from './routes';
-import ToastConainer from './components/common/ToastContainer';
 
 const App: React.FC = () => (
   <>
     <GlobalStyled />
-    <AuthProvider>
+    <AppProvider>
       <Router>
         <Routes />
       </Router>
-    </AuthProvider>
-    <ToastConainer />
+    </AppProvider>
   </>
 );
 
