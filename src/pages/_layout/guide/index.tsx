e2226fbc1fde3f299d316f9dd0a93f2925a93f54
tenclar <tenclarvalus@gmail.com>
2020-11-13 23:49:52 -0500

    layout Guia

--- src/pages/_layout/guide/index.tsx
+++ src/pages/_layout/guide/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
-import { Container } from './styles';
+import { Wrapper, Container } from './styles';
+import Header from '../../../components/guide/Header';
 
-const Guide: React.FC = ({ children }) => <Container>{children}</Container>;
+const Guide: React.FC = ({ children }) => (
+  <Wrapper>
+    <Header />
+    <Container>{children}</Container>;
+  </Wrapper>
+);
 
 export default Guide;
--- src/pages/_layout/guide/styles.ts
+++ src/pages/_layout/guide/styles.ts
@@ -1,6 +1,15 @@
 import styled from 'styled-components';
 
+export const Wrapper = styled.div`
+  /* height: 100vh; */
+  min-height: 100% !important;
+  /*
+  display: flex;
+  flex-direction: column;
+  */
+`;
 export const Container = styled.div`
   flex: 1;
   background-color: green;
+  min-height: 100% !important;
 `;
