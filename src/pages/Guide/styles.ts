133a8141638e12d39b2363e872157fbbaa12a08a
tenclar <tenclarvalus@gmail.com>
2020-11-13 23:51:10 -0500

    Stilo pagina guia

--- src/pages/Guide/styles.ts
+++ src/pages/Guide/styles.ts
@@ -1,7 +1,9 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  min-height: 100vmin;
+  /* min-height: 100vmin; */
+  flex: 1;
+  min-height: 100vh !important;
   display: flex;
   flex-direction: column;
   align-items: center;
