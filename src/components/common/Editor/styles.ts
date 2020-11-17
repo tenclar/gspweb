a355ad3deeecf3d2494c17c54f49ccfdf7fc209f
tenclar <tenclar.silva@ac.gov.br>
2020-11-17 13:59:25 -0500

    componente editor texto

---
+++ src/components/common/Editor/styles.ts
@@ -0,0 +1,74 @@
+import styled, { css } from 'styled-components';
+import ToolTip from '../Tooltip';
+
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+  isErrored: boolean;
+}
+export const Container = styled.div<ContainerProps>`
+  color: #666360;
+  border-radius: 4px;
+  /* border: 2px solid #233129; */
+  border: 1px solid hsl(0,0%,80%);
+  padding: 10px;
+  width: 80%;
+
+  display: flex;
+  align-items: center;
+
+  & + div {
+    margin-top: 8px;
+  }
+
+  ${(props) =>
+    props.isErrored &&
+    css`
+      border-color: #c53030;
+    `}
+
+  ${(props) =>
+    props.isFocused &&
+    css`
+      color: #ff9900;
+      border-color: #ff9000;
+    `}
+
+  ${(props) =>
+    props.isFilled &&
+    css`
+      color: #ff9900;
+    `}
+
+  input {
+    flex: 1;
+    background: transparent;
+    border: 0;
+    color: #000;
+
+    &::placeholder {
+      color: #666360;
+    }
+  }
+
+  > svg {
+    margin-right: 16px;
+  }
+`;
+
+export const Error = styled(ToolTip)`
+  height: 20px;
+  margin-left: 12px;
+
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background: #c53030;
+    color: #fff;
+    &::before {
+      border-color: #c53030 transparent;
+    }
+  }
+`;
