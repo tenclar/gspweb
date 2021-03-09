450737761970d5189a128e6c1dbe9941f00578bf
tenclar <tenclarvalus@gmail.com>
2021-03-09 07:29:03 -0500

    Link  tags, temas pracas locais Cidades

--- src/components/admin/Sidebar/index.tsx
+++ src/components/admin/Sidebar/index.tsx
@@ -17,31 +17,31 @@ const Sidebar: React.FC = () => (
       <LinkButton to="/ad/cadastro/categorias">
         <FaRegCircle /> <div>Categorias</div>
       </LinkButton>
+      <LinkButton to="/ad/cadastro/centrais">
+        <FaRegCircle /> <div>Centrais</div>
+      </LinkButton>
+      <LinkButton to="/ad/cadastro/cidades">
+        <FaRegCircle /> <div>Cidades</div>
+      </LinkButton>
+      <LinkButton to="/ad/cadastro/superiores">
+        <FaRegCircle /> <div>Inst. Superiores</div>
+      </LinkButton>
+      <LinkButton to="/ad/cadastro/locais">
+        <FaRegCircle /> <div>Locais</div>
+      </LinkButton>
+      <LinkButton to="/ad/cadastro/orgaos">
+        <FaRegCircle /> <div>Ogãos</div>
+      </LinkButton>
+      <LinkButton to="/ad/cadastro/pracas">
+        <FaRegCircle /> <div>Praças</div>
+      </LinkButton>
       <LinkButton to="/ad/cadastro/tags">
         <FaRegCircle /> <div>Tags</div>
       </LinkButton>
       <LinkButton to="/ad/cadastro/temas">
         <FaRegCircle /> <div>Temas</div>
       </LinkButton>
-      <LinkButton to="/ad/cadastro/centrais">
-        <FaRegCircle /> <div>Centrais</div>
-      </LinkButton>
-      <LinkButton to="/ad/cadastro/pracas">
-        <FaRegCircle /> <div>Praças</div>
-      </LinkButton>
-      <LinkButton to="/ad/cadastro/superiores">
-        <FaRegCircle /> <div>Inst. Superiores</div>
-      </LinkButton>
-      <LinkButton to="/ad/cadastro/orgaos">
-        <FaRegCircle /> <div>Ogãos</div>
-      </LinkButton>
-      <LinkButton to="/ad/cadastro/locais">
-        <FaRegCircle /> <div>Locais</div>
-      </LinkButton>
 
-      <LinkButton to="/ad/cadastro/cidades">
-        <FaRegCircle /> <div>Cidades</div>
-      </LinkButton>
       <LinkButton to="/ad/cadastro/publicos">
         <FaRegCircle /> <div>Públicos Alvo</div>
       </LinkButton>
